7a5f28fb828395577fde6e78615aaaf411a8408d
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-03-10 20:48:27 +0530

    chore: Moved APi key to env

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, Setdata] = useState([]);
+
+  useEffect(() => {
+    const fectdata = async () => {
+      try {
+        await fetch(
+          `https://api.cricapi.com/v1/currentMatches?apikey=${
+            import.meta.env.VITE_API_KEY
+          }&offset=0`
+        )
+          .then((res) => res.json())
+          .then((dat) => console.log(dat));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fectdata();
+  }, []);
+
   return <div></div>;
 }
 
