73770495d43c4e2b7ada46aa1a2c2d2592163d28
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2025-03-12 21:49:19 +0530

    chore : CricScore Page done

--- src/assets/Pages/CricScore.tsx
+++ src/assets/Pages/CricScore.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+
+type Match = {
+  id: string;
+  dateTimeGMT: string;
+  matchType: string;
+  status: string;
+  ms: string;
+  t1: string;
+  t2: string;
+  t1s: string;
+  t2s: string;
+  t1img: string;
+  t2img: string;
+  series: string;
+};
+
+type data = {
+  matches: Match[];
+  loading: boolean;
+};
+
 const CricScore = () => {
-  return <div>CricScore</div>;
+  const [matches, setMatches] = useState<Match[]>([]);
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      try {
+        const response = await fetch(
+          "https://api.cricapi.com/v1/cricScore?apikey=f82b10e9-e469-4f43-a441-725fef3835f0"
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching match data:", error);
+      } finally {
+      }
+    };
+
+    fetchMatches();
+  }, []);
+  return <div>Cricscore</div>;
 };
 
 export default CricScore;
